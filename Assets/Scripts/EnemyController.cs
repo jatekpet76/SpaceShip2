352513948a5421d388e9fb3163b434e971112b85
Godzzo <egodzzo@gmail.com>
2020-02-17 17:38:48 +0100

    Add EnemyController and repositioning enemies for movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        
+        if (transform.position.y < -4.1f)
+        {
+            transform.position = new Vector3(Random.Range(-9, 9), 6f, -2);
+        }
+    }
+}
