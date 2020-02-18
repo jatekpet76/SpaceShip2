fa428a5df51d1a3fe0be7a2c443799cbeccabd83
Godzzo <egodzzo@gmail.com>
2020-02-18 16:21:21 +0100

    EnemyManager added for spawning random enemy ship, works fine 😆

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _shipPrefabs;
+
+    [SerializeField]
+    private int _idleSecs = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var coroutine = spawnShips();
+        StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator spawnShips()
+    {
+        while (true)
+        {
+            spawnRandomShip();
+
+            yield return new WaitForSeconds(_idleSecs);
+        }
+    }
+
+    private void spawnRandomShip()
+    {
+        var shipPos = Random.Range(0, _shipPrefabs.Length);
+
+        Instantiate(_shipPrefabs[shipPos], new Vector3(Random.Range(-9, 9), 6f, -2f), Quaternion.identity);
+    }
+}
